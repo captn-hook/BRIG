92343609d996a3d18859a11b405ffd0ccdf94adf
capn-hook <tristanskyhook@gmail.com>
2022-07-22 15:35:25 -0700

    fix?

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -10,15 +10,10 @@ module.exports = {
         path: path.resolve(__dirname, '../dist'),
         libraryTarget: "var",
         library: "FileExt",
-<<<<<<< HEAD
         library: "Point",
         library: "Tracer",
         library: "Controls",
         library: "Data"
-=======
-        library: "TracerPoint",
-        library: "SpherePoint"
->>>>>>> main
     },
     devtool: 'source-map',
     plugins: [
