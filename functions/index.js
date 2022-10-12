6a9eceafc12b85b495aee777e5c66037bdd51960
captn-hook <tristanskyhook@gmail.com>
2022-10-11 17:51:51 -0700

    -f late, working on http funcs. gcloud functions initialized and deployed, not verified

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,23 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+functions.https("userList", (req, res) => {
+  const allUsers = [];
+  return admin.auth().listUsers()
+      .then(function(listUsersResult) {
+        listUsersResult.users.forEach(function(userRecord) {
+          // For each user
+          const userData = userRecord.toJSON();
+          allUsers.push(userData);
+        });
+        res.status(200).send(JSON.stringify(allUsers));
+        return functions.logger.log("Sent Users");
+      })
+      .catch(function(error) {
+        console.log("Error listing users:", error);
+        res.status(500).send(error);
+        return functions.logger.log("User List Error");
+      });
+});
